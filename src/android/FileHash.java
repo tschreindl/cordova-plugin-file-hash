80167bdfcb198b25990dd03ae4fd73ffff51d128
Victor Gavro <vgavro@gmail.com>
2021-09-13 16:50:32 +0300

    android: refactored to use thread for digest calculation

--- src/android/FileHash.java
+++ src/android/FileHash.java
@@ -37,131 +37,77 @@ import org.json.JSONException;
 import org.json.JSONObject;
 /**
  *
- * @author LordKBX
+ * @author LordKBX, vgavro
  */
 public class FileHash extends CordovaPlugin {
-
-  private String AppId;
-  private Context context;
-  private String[] errorCodes = {"Execution Error", "Unknown Algorithm",
-                                 "File not found", "File access error",
-                                 "Digest error"};
-
-  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
-    this.webView = webView;
-    Activity activity = cordova.getActivity();
-    this.context = activity.getApplicationContext();
-    this.AppId = context.getPackageName();
-  }
-
   public boolean execute(String action, JSONArray args,
                          CallbackContext callbackContext) throws JSONException {
     String url = args.getString(0);
     url = URLDecoder.decode(url);
-    System.out.println("File root system " + context.getPackageName());
     JSONObject r = new JSONObject();
     String ealgo = "";
-    if (action.equals("md2")) {
-      ealgo = "MD2";
-    }
-    if (action.equals("md5")) {
-      ealgo = "MD5";
-    }
-    if (action.equals("sha1")) {
-      ealgo = "SHA-1";
-    }
-    if (action.equals("sha256")) {
-      ealgo = "SHA-256";
-    }
-    if (action.equals("sha384")) {
-      ealgo = "SHA-384";
-    }
-    if (action.equals("sha512")) {
-      ealgo = "SHA-512";
-    }
-    if (action.equals("md2") || action.equals("md5") || action.equals("sha1") ||
-        action.equals("sha256") || action.equals("sha384") ||
-        action.equals("sha512")) {
-      String[] rez = central(url, ealgo);
-      if (rez[1] == "") {
-        r.put("file", url);
-        r.put("algo", ealgo);
-        r.put("result", rez[0]);
-        callbackContext.success(r);
-      } else {
-        r.put("code", rez[0]);
-        r.put("message", rez[1]);
-        callbackContext.error(r);
-      }
-    } else {
+    if (action.equals("md2")) ealgo = "MD2";
+    else if (action.equals("md5")) ealgo = "MD5";
+    else if (action.equals("sha1")) ealgo = "SHA-1";
+    else if (action.equals("sha256")) ealgo = "SHA-256";
+    else if (action.equals("sha384")) ealgo = "SHA-384";
+    else if (action.equals("sha512")) ealgo = "SHA-512";
+    else {
       r.put("code", 1);
-      r.put("message", this.errorCodes[1]);
+      r.put("message", "Unknown Algorithm");
       callbackContext.error(r);
+      return true;
     }
-    return true;
-  }
+    if (url.contains("file:///android_asset/")) {
+      // LordKBX version copied file, we should omit it with error
+      r.put("code", 5);
+      r.put("message", "Unsupported url");
+      callbackContext.error(r);
+      return true;
+    }
+    url = url.replace("file:///", "/").replace("file:", "");
 
-  // java.io.FileNotFoundException
-  public String[] central(String fileURL, String algo) {
-    FileInputStream fis;
-    String[] ret = {"", ""};
-    try {
-      if (fileURL.contains("file:///android_asset/")) {
-        fileURL = fileURL.replace("file:///android_asset/", "");
-        String[] tab = fileURL.split("/");
-        String endUrl =
-            "/data/data/" + this.AppId + "/cache/" + fileURL.replace("/", "_");
-
-        InputStream in = this.context.getAssets().open(fileURL);
-        OutputStream out = new FileOutputStream(endUrl);
-
-        // Transfer bytes from in to out
-        byte[] buf = new byte[1024];
-        int len;
-        while ((len = in.read(buf)) > 0)
-          out.write(buf, 0, len);
-        in.close();
-        out.close();
-        fileURL = endUrl;
-      } else {
-        fileURL = fileURL.replace("file:///", "/").replace("file:", "");
-      }
-
-      fis = new FileInputStream(fileURL);
-      MessageDigest md = MessageDigest.getInstance(algo);
-      byte[] dataBytes = new byte[1024];
-      int nread = 0;
-      while ((nread = fis.read(dataBytes)) != -1) {
-        md.update(dataBytes, 0, nread);
-      };
-      byte[] mdbytes = md.digest();
-      // convert the byte to hex format method 1
-      StringBuffer sb = new StringBuffer();
-      for (int i = 0; i < mdbytes.length; i++) {
-        sb.append(
-            Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
-      }
-      ret[0] = sb.toString();
-      System.out.println(sb.toString());
-      return ret;
-    } catch (Exception ex) {
-      if (ex instanceof FileNotFoundException) {
-        File f = new File(fileURL);
-        if (f.exists()) {
-          ret[0] = Integer.toString(3);
-          ret[1] = this.errorCodes[3];
-        } else {
-          ret[0] = Integer.toString(2);
-          ret[1] = this.errorCodes[2];
+    cordova.getThreadPool().execute(new Runnable() {
+      public void run() {
+        FileInputStream fis;
+        JSONObject r = new JSONObject();
+        try {
+          fis = new FileInputStream(url);
+          MessageDigest md = MessageDigest.getInstance(algo);
+          byte[] dataBytes = new byte[1024];
+          int nread = 0;
+          while ((nread = fis.read(dataBytes)) != -1) {
+            md.update(dataBytes, 0, nread);
+          };
+          byte[] mdbytes = md.digest();
+          // convert the byte to hex format method 1
+          StringBuffer sb = new StringBuffer();
+          for (int i = 0; i < mdbytes.length; i++) {
+            sb.append(
+                Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+          }
+          r.put("file", url);
+          r.put("algo", ealgo);
+          r.put("result", sb.toString());
+          callbackContext.success(r);
+        } catch (Exception ex) {
+          if (ex instanceof FileNotFoundException) {
+            File f = new File(url);
+            if (f.exists()) {
+              r.put("code", 3);
+              r.put("message", "File access error");
+            } else {
+              r.put("code", 2);
+              r.put("message", "File not found");
+            }
+          } else {
+            r.put("code", 4);
+            r.put("message", "Digest error");
+          }
+          callbackContext.error(r);
         }
-        return ret;
-      } else {
-        System.out.println(ex);
-        ret[0] = Integer.toString(4);
-        ret[1] = this.errorCodes[4];
-        return ret;
       }
-    }
+    });
+    return true;
   }
 }
